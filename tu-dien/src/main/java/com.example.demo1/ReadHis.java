ec9ddf2bd23479ac1ca13832d612c95ad2f67fbd
22022658NguyenTienKhoi <nguyentienkhoi210@gmail.com>
2023-11-22 20:02:20 +0700

    final

--- tu-dien/src/main/java/com.example.demo1/ReadHis.java
+++ tu-dien/src/main/java/com.example.demo1/ReadHis.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReadHis {
-    public List<String> readfile() throws IOException {
+    public List<String> readfile(String user) throws IOException {
         List<String> word = new ArrayList<>();
         String line = null;
-        BufferedReader br = new BufferedReader(new FileReader("D:\\dict\\newdemo\\demo1\\src\\main\\History.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("D:\\demo1\\src\\main\\"+user+"History.txt"));
         while((line = br.readLine()) != null) {
             word.add(line);
         }
         return word;
     }
-    public void writeHis(String content) {
-        String filePath = "D:\\dict\\newdemo\\demo1\\src\\main\\History.txt";
+    public void writeHis(String content,String user) {
+        String filePath = "D:\\demo1\\src\\main\\"+user+"History.txt";
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath,true))) {
             // Ghi nội dung vào tệp tin
