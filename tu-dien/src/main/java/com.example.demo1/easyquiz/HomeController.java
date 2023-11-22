966568bb1c1d31aaa81cd8c10e36a39810932af3
Nguyễn Tiến Khôi <127768809+22022658NguyenTienKhoi@users.noreply.github.com>
2023-11-22 18:08:00 +0700

    Add files via upload

---
+++ tu-dien/src/main/java/com.example.demo1/easyquiz/HomeController.java
@@ -0,0 +1,102 @@
+package com.example.demo1.easyquiz;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class HomeController {
+
+    @FXML
+    private Button playquizbtn;
+    @FXML
+    private Button playquizbtn2;
+    @FXML
+    private Button playquizbtn3;
+    @FXML
+    private Button playquizbtn4;
+    @FXML
+    MenuButton phonetics;
+
+    @FXML
+    private void initialize() {
+        playquizbtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Stage thisstage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+                    thisstage.close();
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("quiz.fxml"));
+                    Scene scene = new Scene(fxmlLoader.load());
+                    Stage stage = new Stage();
+                    stage.setScene(scene);
+                    stage.initStyle(StageStyle.TRANSPARENT);
+                    scene.setFill(Color.TRANSPARENT);
+                    stage.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        playquizbtn2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Stage thisstage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+                    thisstage.close();
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("quiz2.fxml"));
+                    Scene scene = new Scene(fxmlLoader.load());
+                    Stage stage = new Stage();
+                    stage.setScene(scene);
+                    stage.initStyle(StageStyle.TRANSPARENT);
+                    scene.setFill(Color.TRANSPARENT);
+                    stage.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        playquizbtn3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Stage thisstage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+                    thisstage.close();
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("quiz3.fxml"));
+                    Scene scene = new Scene(fxmlLoader.load());
+                    Stage stage = new Stage();
+                    stage.setScene(scene);
+                    stage.initStyle(StageStyle.TRANSPARENT);
+                    scene.setFill(Color.TRANSPARENT);
+                    stage.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        playquizbtn4.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Stage thisstage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+                    thisstage.close();
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("quiz4.fxml"));
+                    Scene scene = new Scene(fxmlLoader.load());
+                    Stage stage = new Stage();
+                    stage.setScene(scene);
+                    stage.initStyle(StageStyle.TRANSPARENT);
+                    scene.setFill(Color.TRANSPARENT);
+                    stage.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
